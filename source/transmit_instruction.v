38387be57cf32597ac0ec1b6eba1e1dd14310c16
NguyenHoanKhanh <khanhvlg12022004@gmail.com>
2025-08-16 22:29:44 +0700

    fix conversation with fetch_instruction

--- source/transmit_instruction.v
+++ source/transmit_instruction.v
@@ -14,37 +14,27 @@ module transmit #(
     output reg t_o_ack;
 
     integer counter;
-    reg accept;
     reg [IWIDTH-1:0] mem_instr [0 : DEPTH - 1];
-
-    initial begin
-        // nạp hex hoặc bin, kèm start-stop
-        $readmemh("./source/instr.txt", mem_instr, 0, DEPTH - 1);
-        counter = 0;
-    end
 
     always @(posedge t_clk or negedge t_rst) begin
         if (!t_rst) begin
             counter   <= 0;
-            t_o_ack   <= 0;
-            accept <= 1'b0;
+            t_o_ack   <= 1'b0;
             t_o_instr <= {IWIDTH{1'b0}};
+            // nạp hex hoặc bin, kèm start-stop
+            $readmemh("./source/instr.txt", mem_instr, 0, DEPTH - 1);
         end
         else begin
-            if (t_i_syn && !accept) begin
+            if (t_i_syn) begin
                 t_o_instr <= mem_instr[counter];
-                t_o_ack   <= 1;
-                accept <= 1'b1;
-                if (counter < DEPTH - 1) begin
-                    counter <= counter + 1;
-                end
-                else begin
-                    counter <= 0;
-                end
-            end
-            else if (!t_i_syn) begin
-                accept <= 1'b0;
-                t_o_ack <= 1'b0;
+                t_o_ack   <= 1'b1;
+                // if (counter < DEPTH - 1) begin
+                //     counter <= counter + 1;
+                // end
+                // else begin
+                //     counter <= 0;
+                // end
+                counter <= (counter < DEPTH - 1) ? counter + 1 : 0;
             end
             else begin
                 t_o_ack <= 1'b0;
