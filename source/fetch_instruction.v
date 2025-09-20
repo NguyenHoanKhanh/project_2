cb2a62688c9f89e6b4cd78d9cb46fa4806156663
NguyenHoanKhanh <khanhvlg12022004@gmail.com>
2025-09-20 15:34:04 +0700

    Two versions, one version has multiple drive but gets all the instruction in the memory, the other is treated as missing one last instruction due to initialization

--- source/fetch_instruction.v
+++ source/fetch_instruction.v
@@ -63,8 +63,8 @@
 //             f_o_syn_r <= 1'b0;
 //             f_o_syn <= 1'b0;
 //             init_done <= 1'b0;
-//             temp_ack <= 1'b0;
-//             temp_last <= 1'b0;
+//             // temp_ack <= 1'b0;
+//             // temp_last <= 1'b0;
 //             req <= 1'b0;
 //             issued_pc <= {PC_WIDTH{1'b0}};
 //         end
@@ -230,8 +230,8 @@ module instruction_fetch #(
             f_o_syn_r <= 1'b0;
             f_o_syn <= 1'b0;
             init_done <= 1'b0;
-            temp_ack <= 1'b0;
-            temp_last <= 1'b0;
+            // temp_ack <= 1'b0;
+            // temp_last <= 1'b0;
             req <= 1'b0;
             issued_pc <= {PC_WIDTH{1'b0}};
         end
